3dd557fb339d172f3c7b51a82ddd717f48e42143
lata002 <158074309+lata002@users.noreply.github.com>
2024-01-31 01:27:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "azurerm_resource_group" "rg" {
 #create subnet
 resource "azurerm_subnet" "subnet" {
  name = "webapps-subnet"
- resource_group_name = azurerm_resource_group.rg.name
+ resource_group_name = data.azurerm_resource_group.rg.name
  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes = ["10.0.0.0/24"]
   }
@@ -24,8 +24,8 @@ resource "azurerm_subnet" "subnet" {
 # Private IP[network interface]
 resource "azurerm_network_interface" "nic" {
  name = "webapp-nic"
- location = azurerm_resource_group.rg.location
- resource_group_name = azurerm_resource_group.rg.name
+ location = data.azurerm_resource_group.rg.location
+ resource_group_name = data.azurerm_resource_group.rg.name
  
  ip_configuration {
   name = "webapp-ip"
@@ -47,8 +47,8 @@ resource "azurerm_public_ip" "public_ip" {
 
 resource "azurerm_linux_virtual_machine" "vm" {
  name = "webapp-vm"
- location =  azurerm_resource_group.rg.location
- resource_group_name = azurerm_resource_group.rg.name
+ location =  data.azurerm_resource_group.rg.location
+ resource_group_name = data.azurerm_resource_group.rg.name
  size = "Standard_B1s"
  admin_username = "adminuser"
  network_interface_ids = [
